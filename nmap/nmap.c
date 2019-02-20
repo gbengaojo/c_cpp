b79d3641709b8e670ce7e3b7e4eae323fb5bb994
Gbenga Ojo <gbenga@gbengaojo.com>
2019-02-19 22:18:19 -0600

    inllne functions

--- nmap/nmap.c
+++ nmap/nmap.c
@@ -223,3 +223,34 @@ int main(int argc, char *argv[]) {
 }
 
 return 0;
+
+__inline__ int unblock_socket(int sd) {
+  int options;
+  /* Unblock our socket to prevent recv from blocking forever
+     on certain target ports. */
+  options = O_NONBLOCK | fcntl(sd, F_GETFL);
+  fcntl(sd, F_SETFL, options);
+  return 1;
+}
+
+__inline__ int block_socket(int sd) {
+  int options;
+  options = (~O_NONBLOCK) & fcntl(sd, F_GETFL);
+  fctnl(sd, F_SETFL, options);
+  return 1;
+}
+
+/* Currently only sets S0_LINGER, I haven't seen any evidence that this
+   helps. I'll do more testing before dumping it. */
+__inline__ void init_socket(int sd) {
+  struct linger l;
+
+  l.l_onoff = 1;
+  l.l_linger = 0;
+
+  if (setsockopt(sd, SOL_SOCKET, SO_LINGER, &l, sizeof(struct linger)))
+  {
+    fprintf(stderr, "Problem setting socket SO_LINGER, errno: %d\n", errno);
+    perror("setsockopt");
+  }
+}
